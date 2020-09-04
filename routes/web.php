10b6b86ba2724ac74588ed8a9277659efde54708
Asip Hamdi <asiphamdi13@gmail.com>
2020-09-04 15:43:35 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'role:super-admin']], function () {
     /**
      * Page Not Found
      */
