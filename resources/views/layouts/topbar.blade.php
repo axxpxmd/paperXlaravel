eeb75849390c441c46905c26895663b88d575f20
Asip Hamdi <asiphamdi13@gmail.com>
2020-09-15 08:36:30 +0700

    update

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -135,7 +135,7 @@
                         <div class="dropdown-menu p-4 dropdown-menu-right" style="width:255px">
                             <div class="box justify-content-between">
                                 <div class="col">
-                                    <a href="{{ route('blank-page') }}">
+                                    <a href="{{ route('master-profile.profile.index') }}">
                                         <i class="icon-user amber-text lighten-2 avatar r-5"></i>
                                         <div class="pt-1">Profil</div>
                                     </a>
